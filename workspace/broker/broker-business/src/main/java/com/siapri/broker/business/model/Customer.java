9163b799f6faf1631752ff26371338e70634e030
sdiabate <diabate_seriba@yahoo.fr>
2017-08-09 00:00:07 +0200

    Add rich client app project

--- workspace/broker/broker-business/src/main/java/com/siapri/broker/business/model/Customer.java
+++ workspace/broker/broker-business/src/main/java/com/siapri/broker/business/model/Customer.java
@@ -15,63 +15,60 @@ import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "CUSTOMER")
-public class Customer extends AbstractEntity{
-
-
-	private static final long serialVersionUID = 1L;
+public class Customer extends AbstractDocumentProvider {
 	
+	private static final long serialVersionUID = 1L;
+
 	@NotNull
-	@Column(name= "FIRST_NAME", nullable = false)
+	@Column(name = "FIRST_NAME", nullable = false)
 	@Size(min = 1)
 	private String firstName;
-	
+
 	@NotNull
 	@Column(name = "LAST_NAME", nullable = false)
 	@Size(min = 1)
 	private String lastName;
-	
+
 	@NotNull
 	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinColumn(name = "CUSTOMER_ID", referencedColumnName = "ID")
 	private List<Address> addresses = new ArrayList<>();
-	
+
 	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinColumn(name = "SINISTER_ID", referencedColumnName = "ID")
-	private List<Sinister> sinisters = new ArrayList<>();
-	
-	public Customer(){}
-	
-	public Customer(String firstName, String lastName, List<Address> addresses){
+	private final List<Sinister> sinisters = new ArrayList<>();
+
+	public Customer() {
+	}
+
+	public Customer(final String firstName, final String lastName, final List<Address> addresses) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.addresses = addresses;
 	}
-
+	
 	public String getFirstName() {
 		return firstName;
 	}
-
-	public void setFirstName(String firstName) {
+	
+	public void setFirstName(final String firstName) {
 		this.firstName = firstName;
 	}
-
+	
 	public String getLastName() {
 		return lastName;
 	}
-
-	public void setLastName(String lastName) {
+	
+	public void setLastName(final String lastName) {
 		this.lastName = lastName;
 	}
-
+	
 	public List<Address> getAddresses() {
 		return addresses;
 	}
-
-	public void setAddresses(List<Address> addresses) {
+	
+	public void setAddresses(final List<Address> addresses) {
 		this.addresses = addresses;
 	}
-
-	
-	
 	
 }
---
+++ workspace/broker/broker-business/src/main/java/com/siapri/broker/business/model/IDocumentProvider.java
@@ -0,0 +1,9 @@
+package com.siapri.broker.business.model;
+
+import java.util.List;
+
+public interface IDocumentProvider {
+	
+	List<Document> getDocuments();
+	
+}

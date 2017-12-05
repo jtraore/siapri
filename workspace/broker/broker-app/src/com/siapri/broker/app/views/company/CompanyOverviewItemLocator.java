aa9a3e551d476fa409836435aabf32afbad5c206
sdiabate <diabate_seriba@yahoo.fr>
2017-12-05 23:35:44 +0100

    Fix bug in enterprises and insurers locators

--- workspace/broker/broker-app/src/com/siapri/broker/app/views/company/CompanyOverviewItemLocator.java
+++ workspace/broker/broker-app/src/com/siapri/broker/app/views/company/CompanyOverviewItemLocator.java
@@ -1,16 +1,10 @@
 package com.siapri.broker.app.views.company;
 
 import com.siapri.broker.app.Activator;
-import com.siapri.broker.app.views.PartView;
 import com.siapri.broker.app.views.common.datalist.DataListItemLocator;
 import com.siapri.broker.business.model.Company;
 
 public abstract class CompanyOverviewItemLocator extends DataListItemLocator<Company> {
-	
-	@Override
-	protected Class<? extends PartView<Company>> getPartViewClass() {
-		return CompanyView.class;
-	}
 	
 	@Override
 	protected String getToolItemId() {
--- workspace/broker/broker-app/src/com/siapri/broker/app/views/insurer/InsurerOverviewItemLocator.java
+++ workspace/broker/broker-app/src/com/siapri/broker/app/views/insurer/InsurerOverviewItemLocator.java
@@ -1,6 +1,8 @@
 package com.siapri.broker.app.views.insurer;
 
+import com.siapri.broker.app.views.PartView;
 import com.siapri.broker.app.views.company.CompanyOverviewItemLocator;
+import com.siapri.broker.business.model.Company;
 
 public class InsurerOverviewItemLocator extends CompanyOverviewItemLocator {
 	
@@ -9,4 +11,13 @@ public class InsurerOverviewItemLocator extends CompanyOverviewItemLocator {
 		return "insurers";
 	}
 
+	@Override
+	protected String getPartName() {
+		return getItemName();
+	}
+	
+	@Override
+	protected Class<? extends PartView<Company>> getPartViewClass() {
+		return InsurerView.class;
+	}
 }
